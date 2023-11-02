cccf303e965721e06e6c3f902f3de032256104c9
Ayush Shete <ayushete2005@gmail.com>
2023-11-03 02:32:53 +0530

    format message and add notification

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf, Markup } = require('telegraf');
 
-const bot = new Telegraf("6374334311:AAHl_pN-89lSK_o1ITPGNrQCbhIFiwwtZl4"); // Replace with your bot token
+const Token = '6374334311:AAHl_pN-89lSK_o1ITPGNrQCbhIFiwwtZl4'
+const bot = new Telegraf(Token); // Replace with your bot token
 
 bot.start((ctx) => ctx.reply('Welcome!!'));
 
@@ -8,46 +9,72 @@ bot.on('sticker', (ctx) => ctx.reply('👍'));
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-// Define a catch-all command to search for specific data
+bot.hears('notify', async (ctx) => {
+    try {
+      // Define your custom message
+      const customMessage = "This is a custom notification message.";
+  
+      // Replace the chat_id with your target chat ID
+      const chatId = -1002091612309; // Replace with your target chat ID
+  
+      // Build the URL for sending a notification
+      const apiUrl = `https://api.telegram.org/bot${Token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(customMessage)}`;
+  
+      // Send the notification
+      const response = await fetch(apiUrl);
+  
+      if (response.ok) {
+        ctx.reply('Custom notification sent successfully!');
+      } else {
+        ctx.reply('Failed to send a custom notification.');
+      }
+    } catch (error) {
+      console.error('Error sending custom notification:', error);
+      ctx.reply('An error occurred while sending the custom notification.');
+    }
+  });
+
 bot.on('text', async (ctx) => {
     const userMessage = ctx.message.text;
     if (userMessage.startsWith('/')) {
-        const command = userMessage.slice(1); // Remove the leading '/'
+        const command = userMessage.slice(1);
         try {
             const response = await fetch('https://api.binance.com/api/v3/ticker/24hr');
             if (response.ok) {
                 const data = await response.json();
                 const coinData = data.find(item => item.symbol === `${command.toUpperCase()}`);
                 if (coinData) {
-                    const message = `
-Market             |  ${coinData.symbol}
--------------------- | ---------
-Last Traded    |  ${coinData.lastPrice}
-24hr change   |  ${coinData.priceChangePercent >= 0 ? '📈 +' : '📉 -'} ${coinData.priceChangePercent}% ${coinData.priceChangePercent >= 0 ? '⬆️' : '⬇️'}
-
-Buy at              |  ${coinData.askPrice}
-Sell at               |  ${coinData.bidPrice}
-24hr Low         |  ${coinData.lowPrice}
-24hr High        |  ${coinData.highPrice}
+                    const message = `<pre>
+Market       |  ${coinData.symbol}
+---- ------- | --------------
+Last Traded  |  ${coinData.lastPrice}
+24hr change  |  ${coinData.priceChangePercent >= 0 ? '+' : ''} ${coinData.priceChangePercent}% ${coinData.priceChangePercent >= 0 ? '⬆️' : '⬇️'}
+</pre>
+<pre>
+Buy at       |  ${coinData.askPrice}
+Sell at      |  ${coinData.bidPrice}
+24hr Low     |  ${coinData.lowPrice}
+24hr High    |  ${coinData.highPrice}
 24Hr Volume  |  ${coinData.volume}
-                    `;
-                    
-            
-                    // Create an inline keyboard button with a link to Binance
+</pre>`;
+
                     const binanceLink = `https://www.binance.com/en/trade/${coinData.symbol}`;
                     const keyboard = Markup.inlineKeyboard([Markup.button.url('Trade on Binance', binanceLink)]);
             
-                    ctx.reply(message, keyboard);
+                    ctx.replyWithHTML(message, keyboard);
                   } else {
                     ctx.reply('Cryptocurrency not found or data not available.');
                   }
             }
-        } catch (error) {
+        } catch (error) {   
             console.error('Error fetching data:', error);
             ctx.reply('An error occurred while fetching data.');
         }
     }
 });
+
+
+  
 
 bot.launch();
 
